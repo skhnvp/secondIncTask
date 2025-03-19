5086a0cf8b5f48e170a44b78327429da0ec063d4
pavelsukhanov <skhnvp@gmail.com>
2025-03-19 16:25:47 +0300

    init

---
+++ src/main/java/ru/stepup/inc/Square.java
@@ -0,0 +1,33 @@
+package ru.stepup.inc;
+
+public class Square {
+    private Point topLeftPoint;
+    private int sideLenght;
+
+    public Square(int x, int y, int sideLenght) {
+        this.topLeftPoint = new Point(x, y);
+        this.sideLenght = sideLenght;
+    }
+
+    public Point getTopLeftPoint() {
+        return new Point(topLeftPoint.getX(), topLeftPoint.getY());
+    }
+
+    public void setTopLeftPoint(int x, int y) {
+        this.topLeftPoint = new Point(x, y);
+    }
+
+    public int getSideLenght() {
+        return sideLenght;
+    }
+
+    public void setSideLenght(int sideLenght) {
+        this.sideLenght = sideLenght;
+    }
+
+    @Override
+    public String toString() {
+        return "Квадрат в точке " + topLeftPoint
+                + " со стороной " + sideLenght;
+    }
+}
